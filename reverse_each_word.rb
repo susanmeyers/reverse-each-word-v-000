805df4eb13d9215ae3d5266ee0f1e3a077bba08f
Susan Meyers <smmeyers24@gmail.com>
2018-08-18 03:35:45 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+require 'pry'
+def reverse_each_word(sentence)
+     sentence.split.map {|word| word.reverse}.join(" ")
+end
+
+
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
+end
+
+
+     #.join(" ")
